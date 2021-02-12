2df819fc5b5864103fae36414446d87d174f78f7
Alberto <albertodelviejosanchez@gmail.com>
2021-02-12 22:23:02 +0100

    Added return text voice

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import speech_recognition as sr
 
 def audio_to_text():
-    obtain_audio()
+    return obtain_audio()
 
 def obtain_audio():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Say something")
         audio = r.listen(source)
-        recognize_speech_google(audio, r)
+    return recognize_speech_google(audio, r)
 
 def recognize_speech_google(audio, r):
     GOOGLE_CLOUD_SPEECH_CREDENTIALS = """credendiales de google"""
     try:
-        print("Creo que has dicho " + r.recognize_google(audio)) 
+        text_detected = r.recognize_google(audio)
+        print("Creo que has dicho " + text_detected) 
     except sr.UnknownValueError:
         print("No lo he entendido")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    return text_detected
 
 audio_to_text()
