6a687fdef09845e3ba34beae1a167ecdced417b8
Alberto <albertodelviejosanchez@gmail.com>
2021-02-12 22:17:47 +0100

    Voice to text functionality added+

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
 import unittest
+import speech_recognition as sr
+
+import main as main
+
+class TestVoiceRecognition(unittest.TestCase):
+
+    def test_voice_recognition(self):
+        r = sr.Recognizer()
+        audio = r.listen(source)
+        self.assertNotNull(audio)
+
+    def test_input(self):
+        pass
+
+    if __name__ == "__main__":
+        unittest.main()
